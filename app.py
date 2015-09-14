9e2d50cf1523ecdcec786ee0de914f497244efc3
James Cox <james@actual-experience.com>
2015-09-14 18:07:52 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from utils import create_event, get_credentials, list_events
+from flask import Flask, request, jsonify
+from apiclient.http import HttpError
+
+app = Flask(__name__)
+app.google_credentials = get_credentials()
+
+
+APP_TOKENS = ["9u4Vj53UwTsSDxBxSPHkrTfJ", "nO5v58iT7qKgXJTqXc3zSEYt"]
+
+
+event = {
+  'summary': 'Test',
+  'start': {
+    'date': '2015-09-14'
+  },
+  'end': {
+    'date': '2015-09-15'
+  }
+}
+
+
+@app.route("/create", methods=['POST'])
+def create():
+    if request.form['token'] in APP_TOKENS:
+        try:
+            calendar, event['summary'], event['start']['date'], event['end']['date'] = request.form['text'].split(',')
+        except ValueError:
+            return "Error - requires three values"
+        
+        if calendar.lower() in ['bristol', 'notts']:
+            if calendar.lower() == 'notts':
+                return "Notts calendar not yet ready."
+            try:
+                return "Gig added! <{result}|Click here> for details!".format(
+                    result=create_event(calendar, event)
+                )
+            except HttpError as e:
+                print e
+                return "Failed to add gig :( Check calendar permissions."
+    return "Nope :|"
+
+
+@app.route("/list", methods=['POST'])
+def list():
+    if request.form['token'] in APP_TOKENS:
+        try:
+            calendar, date = request.form['text'].split(',')
+        except ValueError:
+            return "Error - requires two values (calendar name and date)"
+        if calendar.lower() in ['bristol', 'notts']:
+            try:
+                events = list_events(calendar, date)
+                return '\n'.join(
+                    [
+                        "{0}, {1}".format(event['start'].get('dateTime', event['start'].get('date')), event['summary'])
+                        for event in events
+                    ]
+                ) or 'No events!'
+            except HttpError as e:
+                print e
+                return "Failed to list gigs :( Check calendar permissions."
+    return "Nope :|"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ utils.py
@@ -0,0 +1,88 @@
+import os
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+from apiclient import discovery
+import httplib2
+
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gigs Calendar'
+API_KEY = 'AIzaSyDtBGVFvMk3LRxPytNmB2z0gTUs4d077Qw'
+
+
+CALENDARS = {
+    'bristol': '8bog3m0l9cfgt48cbk3lgdk8o0@group.calendar.google.com',
+    'notts': '1gghroldtmjh8mvuo63s33r2uk@group.calendar.google.com',
+}
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatability with Python 2.6
+            credentials = tools.run(flow, store)
+        print 'Storing credentials to ' + credential_path
+    return credentials
+
+
+def create_event(credentials, calendar, event):
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http, developerKey=API_KEY)
+
+    for k, v in event.items():
+        if isinstance(v, dict):
+            for kk, vv in v.items():
+                event[k][kk] = vv.strip()
+        else:
+            event[k] = v.strip()
+
+    eventsResult = service.events().insert(
+        calendarId=CALENDARS[calendar.strip().lower()], 
+        body=event
+    ).execute()
+    return eventsResult.get('htmlLink')
+
+
+def list_events(credentials, calendar, date):
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http, developerKey=API_KEY)
+
+    start = '{date}T00:00:00+01:00'.format(date=date.strip())
+    end = '{date}T23:59:59+01:00'.format(date=date.strip())
+
+    eventsResult = service.events().list(
+        calendarId=CALENDARS[calendar.strip().lower()], 
+        timeMin=str(start), timeMax=str(end), 
+        maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    return eventsResult.get('items', [])
+    

0190a53f3fd091ff7d1545efce17939ba9798d74
James Cox <james@actual-experience.com>
2015-09-22 17:43:25 +0100

    Changed to a single endpoint

--- app.py
+++ app.py
@@ -7,17 +7,15 @@ from apiclient.http import HttpError
 FORMAT = "%(asctime)-15s: %(levelname)s: %(message)s"
 logging.basicConfig(format=FORMAT, filename="gigs.log", level=logging.INFO)
 
-
 app = Flask(__name__)
 app.google_credentials = get_credentials()
-
 
 APP_TOKENS = [
     val
     for key, val in os.environ.items()
     if key.startswith('APP_TOKEN')
 ]
-
+CALENDARS = ['bristol', 'notts']
 
 event = {
   'summary': 'Test',
@@ -30,68 +28,77 @@ event = {
 }
 
 
+def add_gig(calendar, text):
+    try:
+        return "Gig added! <{result}|Click here> for details!".format(
+            result=quick_create_event(
+                app.google_credentials, 
+                calendar, 
+                text
+            )
+        )
+    except HttpError as e:
+        logging.exception(str(e))
+        return "Failed to add gig :( Check calendar permissions."
+
+
+def list_gigs(calendar, date):
+    try:
+        events = list_events(app.google_credentials, calendar, date)
+        return '\n'.join(
+            [
+                "<{0}|{1} @ {2}>"
+                .format(
+                    event.get('htmlLink'),
+                    event['summary'],
+                    event.get('location', calendar.title())
+                )
+                for event in events
+            ]
+        ) or 'No events!'
+    except HttpError as e:
+        logging.exception(str(e))
+        return "Failed to list gigs :( Check calendar permissions."
+
+
 @app.route("/")
 def index():
     return 'doom'
 
 
-@app.route("/create", methods=['POST'])
+@app.route("/gig", methods=['POST'])
 def create():
+    
+    def get_args(request, func_name):
+        return request.form['text'][len(func_name) + 1:].split(',')
+
+    def get_func(func_name):
+        return {
+            'add': add_gig,
+            'list': list_gigs,
+        }[func_name]
+
+    def get_fname(request):
+        return request.form['text'].split(' ')[0]
+
     if request.form.get('token') in APP_TOKENS:
         try:
-            calendar, text = request.form['text'].split(',')
+            fname = get_fname(request)
+            function = get_func(fname)
+            calendar, arg = get_args(request, fname)
+            if calendar.lower() in CALENDARS:
+                return function(calendar, arg)
+            else:
+                return "Invalid calendar"
         except (ValueError, KeyError) as e:
             logging.exception(str(e))
-            return "Error - requires two values (calendar and text)"
-        if calendar.lower() in ['bristol', 'notts']:
-            try:
-                return "Gig added! <{result}|Click here> for details!".format(
-                    result=quick_create_event(
-                        app.google_credentials, 
-                        calendar, 
-                        text
-                    )
-                )
-            except HttpError as e:
-                logging.exception(str(e))
-                return "Failed to add gig :( Check calendar permissions."
+            return "Error - requires three values ([option] [calendar], text)"
     else:
         logging.error("invalid token: %s", request.form.get('token'))
     return "Nope :|"
-
-
-@app.route("/list", methods=['POST'])
-def list():
-    if request.form.get('token') in APP_TOKENS:
-        try:
-            calendar, date = request.form['text'].split(',')
-        except (ValueError, KeyError) as e:
-            logging.exception(str(e))
-            return "Error - requires two values (calendar name and date)"
-        if calendar.lower() in ['bristol', 'notts']:
-            try:
-                events = list_events(app.google_credentials, calendar, date)
-                return '\n'.join(
-                    [
-                        "<{0}|{1} @ {2}>"
-                        .format(
-                            event.get('htmlLink'),
-                            event['summary'],
-                            event.get('location', calendar.title())
-                        )
-                        for event in events
-                    ]
-                ) or 'No events!'
-            except HttpError as e:
-                logging.exception(str(e))
-                return "Failed to list gigs :( Check calendar permissions."
-    else:
-        logging.error("invalid token: %s", request.form.get('token'))
-    return "Nope :|"
-
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=os.environ.get('GIG_DEBUG', False))
+    app.run(debug=os.environ.get('GIG_DEBUG', True))
 
 
